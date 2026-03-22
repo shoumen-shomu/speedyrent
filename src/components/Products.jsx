fc48a5f700046a8001a30f1e49160d0a009bacad
Shoumen Shomu <shoumenshomu@gmail.com>
2026-03-22 18:41:04 +0600

    Add product image and integrate into Products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Images from './Images'
+import productImg from "/src/assets/productImg.png"
 
 const Products = () => {
   return (
     <>
-    <div className=""></div>
+    <div className="">
+      <Images imgSrc={productImg}/>
+    </div>
     </>
   )
 }
