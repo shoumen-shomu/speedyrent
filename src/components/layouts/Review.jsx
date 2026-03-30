f21fbd43faef6fab00bcea6b2532827ce83767b9
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-30 23:46:06 +0600

    review heading done

--- src/components/layouts/Review.jsx
+++ src/components/layouts/Review.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import Container from "../Container";
 
 const Review = () => {
   return (
     <>
-    <section>
-      
-    </section>
+      <section className="mt-87.5">
+        <Container>
+          <div className="flex">
+            <div className="">
+              <h1 className="font-pop font-semibold text-[48px] text-[#181818] leading-[150%] w-[660px]">See Why Speedy is the Top Choice for Car Rentals</h1>
+            </div>
+          </div>
+        </Container>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
