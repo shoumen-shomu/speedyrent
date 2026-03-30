c9bb4a8703e0306c75edd533475f8e4e7b9db69d
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-30 23:58:01 +0600

    review text added

--- src/components/layouts/Review.jsx
+++ src/components/layouts/Review.jsx
@@ -17,6 +17,18 @@ const Review = () => {
                 amet molestie imperdiet quis enim varius. Vitae rutrum praesent
                 a et cursus sagittis egestas blandit eget.{" "}
               </p>
+              {/* Review Start  */}
+              <div className="w-168.5 mt-[50px]">
+                <p className="font-pop font-normal text-[24px] text-[#181818] leading-[180%] italic mb-[30px]">
+                  "I recently rented a car from Speedy and I was blown away by
+                  the level of service I received. The booking process was quick
+                  and easy, and the staff were friendly and helpful when I
+                  arrived to pick up my rental car”F
+                </p>
+                <h3>James Andrew</h3>
+                <p>Customer</p>
+              </div>
+              {/* Review End */}
             </div>
           </div>
         </Container>
