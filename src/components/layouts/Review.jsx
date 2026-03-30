fb867698b0e9ccd166417d968eed37119c9fe72d
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-30 23:49:07 +0600

    review p added

--- src/components/layouts/Review.jsx
+++ src/components/layouts/Review.jsx
@@ -8,7 +8,14 @@ const Review = () => {
         <Container>
           <div className="flex">
             <div className="">
-              <h1 className="font-pop font-semibold text-[48px] text-[#181818] leading-[150%] w-[660px]">See Why Speedy is the Top Choice for Car Rentals</h1>
+              <h1 className="font-pop font-semibold text-[48px] text-[#181818] leading-[150%] w-[660px] mb-[30px]">
+                See Why Speedy is the Top Choice for Car Rentals
+              </h1>
+              <p className="font-pop font-normal text-base text-[#454545] leading-[180%] w-[594px]">
+                Lorem ipsum dolor sit amet consectetur. Purus viverra viverra
+                amet molestie imperdiet quis enim varius. Vitae rutrum praesent
+                a et cursus sagittis egestas blandit eget.{" "}
+              </p>
             </div>
           </div>
         </Container>
