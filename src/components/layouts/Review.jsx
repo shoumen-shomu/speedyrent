4eda58f2384c7dbea03c6db5ce4147796cef50e4
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-02 19:14:43 +0600

    Review Part Done

--- src/components/layouts/Review.jsx
+++ src/components/layouts/Review.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Container from "../Container";
+import Images from "../Images";
+import reviewImage from "/src/assets/testimonialImage.png";
+import { MdChevronRight } from "react-icons/md";
 
 const Review = () => {
   return (
     <>
       {/* review part Start */}
-      <section className="mt-87.5">
+      <section className="mt-87.5 mb-87.5">
         <Container>
-          <div className="flex">
-            <div className="gap-[140px]">
+          <div className="flex gap-35 items-center">
+            <div className="">
               <h1 className="font-pop font-semibold text-[48px] text-[#181818] leading-[150%] w-[660px] mb-[30px]">
                 See Why Speedy is the Top Choice for Car Rentals
               </h1>
@@ -25,11 +28,22 @@ const Review = () => {
                   and easy, and the staff were friendly and helpful when I
                   arrived to pick up my rental car”F
                 </p>
-                <h3 className="font-pop font-semibold text-[32px] text-[#181818] leading-[150%]">James Andrew</h3>
-                <p className="font-pop font-semibold text-base text-[#454545] leading-[150%]">Customer</p>
+                <h3 className="font-pop font-semibold text-[32px] text-[#181818] leading-[150%]">
+                  James Andrew
+                </h3>
+                <p className="font-pop font-semibold text-base text-[#454545] leading-[150%]">
+                  Customer
+                </p>
+                {/* <div className="flex gap-10">
+                  <div className="w-15 h-15 bg-[#EA002D] flex items-center justify-center">
+                    <MdChevronRight className="text-white text-[15px]" />
+                  </div>
+                </div> */}
               </div>
               {/* Review End */}
-              
+            </div>
+            <div className="">
+              <Images imgSrc={reviewImage} imgAlt={reviewImage} />
             </div>
           </div>
         </Container>
